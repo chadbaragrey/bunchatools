ac6de869e33034fc0f3d31fa40c8d8da37057c5f
ChrisWSchultz <KamrisAjelico@gmail.com>
2014-05-23 13:49:01 -0600

    Update blc.py
    Added support for an external file with all of the black list sites

--- blc.py
+++ blc.py
@@ -1,16 +1,27 @@
 # Python Black List checker - Prototype
-# Version 0.0.1
+# Version 0.0.2
 
-import socket, re
-
-blacklists = ['b.barracudacentral.org', 'dnsbl.anticaptcha.net']
-ip = raw_input('Enter an IP address: ')
+import re, socket
+	
+#globals
+blacklists = []
+#ip = raw_input('Enter an IP address: ')
+ip = '127.0.0.2' #test ip, shows as on the blacklist
 ip_rev = ''
 
+#open file with all of the blacklist sites to check.
+#turn the file into a list and remove the newline characters
+blacklists = open('blacklist_sites', 'r').readlines()
+for index in range(len(blacklists)):
+	blacklists[index] = blacklists[index].rstrip('\n')
+	
+#Reverse IP address
 def reverse_ip(ip):
 	global ip_rev
 	ip_rev = '.'.join(re.split('\.', ip)[::-1])
 
+#Check our IP against a list of blacklists, 
+#returns a message if it is on the list or if the BL hostname is invalid.
 def blacklist_check(ip, ip_rev):
 	for blist in blacklists:
 		try:
@@ -18,7 +29,7 @@ def blacklist_check(ip, ip_rev):
 			print 'IP: ' + ip + ' is on the ' + blist + ' blacklist.'
 			
 		except socket.gaierror:
-			pass
+			print 'Hostname error.'
 
 reverse_ip(ip)
 blacklist_check(ip, ip_rev)
